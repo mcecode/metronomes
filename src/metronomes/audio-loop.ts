98729c63fe19f1dc344a9e5fbbade32aab785c08
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2024-03-14 05:40:45 +0800

    Implement AudioLoop

--- src/metronomes/audio-loop.ts
+++ src/metronomes/audio-loop.ts
@@ -1,34 +1,61 @@
 import type Metronome from "./metronome.ts";
 
 export default class AudioLoop implements Metronome {
-  #isPlaying: boolean = false;
-  #bpm: number = 135;
+  #isPlaying = false;
+  #audioContext = new AudioContext();
+  #sourceNode = new AudioBufferSourceNode(this.#audioContext, {
+    loop: true,
+    loopEnd: 60 / 135
+  });
 
   constructor() {
-    // TODO
+    this.#init();
+  }
+  async #init() {
+    const offlineAudioContext = new OfflineAudioContext({
+      numberOfChannels: 1,
+      length: this.#audioContext.sampleRate * 2,
+      sampleRate: this.#audioContext.sampleRate
+    });
+    const oscillatorNode = new OscillatorNode(offlineAudioContext, {
+      // E4
+      frequency: 330
+    });
+    const gainNode = new GainNode(offlineAudioContext);
+
+    gainNode.gain.linearRampToValueAtTime(1, 0);
+    gainNode.gain.linearRampToValueAtTime(0, 0.03);
+
+    oscillatorNode.connect(gainNode);
+    gainNode.connect(offlineAudioContext.destination);
+
+    oscillatorNode.start(0);
+    oscillatorNode.stop(0.03);
+
+    this.#sourceNode.buffer = await offlineAudioContext.startRendering();
+    this.#sourceNode.connect(this.#audioContext.destination);
+    this.#sourceNode.start();
   }
 
-  play() {
+  async play() {
     if (this.#isPlaying) {
       return;
     }
 
-    // TODO
-
+    await this.#audioContext.resume();
     this.#isPlaying = true;
   }
 
-  pause() {
+  async pause() {
     if (!this.#isPlaying) {
       return;
     }
 
-    // TODO
-
+    await this.#audioContext.suspend();
     this.#isPlaying = false;
   }
 
   updateBpm(bpm: number) {
-    this.#bpm = bpm;
+    this.#sourceNode.loopEnd = 60 / bpm;
   }
 }
