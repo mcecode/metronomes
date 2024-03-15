0b36fd29682d100236ed2cd95942c9303af899e8
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2024-03-16 03:36:24 +0800

    Implement LookaheadTimer

--- src/metronomes/lookahead-timer.ts
+++ src/metronomes/lookahead-timer.ts
@@ -1,34 +1,66 @@
 import type Metronome from "./metronome.ts";
 
 export default class LookaheadTimer implements Metronome {
-  #isPlaying: boolean = false;
-  #bpm: number = 135;
+  #isPlaying = false;
+  #audioContext = new AudioContext();
+  #intervalId = 0;
+  #nextNoteTime = 0;
+  #secondsPerBeat = 60 / 135;
 
-  constructor() {
-    // TODO
-  }
-
-  play() {
+  async play() {
     if (this.#isPlaying) {
       return;
     }
 
-    // TODO
+    await this.#audioContext.resume();
+
+    this.#nextNoteTime = this.#audioContext.currentTime;
+    this.#oscillate();
+    setInterval(this.#schedule, 25);
 
     this.#isPlaying = true;
   }
 
-  pause() {
+  async pause() {
     if (!this.#isPlaying) {
       return;
     }
 
-    // TODO
+    await this.#audioContext.suspend();
+
+    clearInterval(this.#intervalId);
 
     this.#isPlaying = false;
   }
 
   updateBpm(bpm: number) {
-    this.#bpm = bpm;
+    this.#secondsPerBeat = 60 / bpm;
+  }
+
+  // Uses the arrow function syntax so that 'this' still works when it is passed
+  // to 'setInterval' as a callback.
+  #schedule = () => {
+    // Schedule all oscillations that should happen within the next 100ms.
+    while (this.#nextNoteTime < this.#audioContext.currentTime + 0.1) {
+      this.#oscillate();
+      this.#nextNoteTime += this.#secondsPerBeat;
+    }
+  };
+
+  #oscillate() {
+    const oscillatorNode = new OscillatorNode(this.#audioContext, {
+      // E4
+      frequency: 330
+    });
+    const gainNode = new GainNode(this.#audioContext);
+
+    gainNode.gain.linearRampToValueAtTime(1, this.#nextNoteTime);
+    gainNode.gain.linearRampToValueAtTime(0, this.#nextNoteTime + 0.03);
+
+    oscillatorNode.connect(gainNode);
+    gainNode.connect(this.#audioContext.destination);
+
+    oscillatorNode.start(this.#nextNoteTime);
+    oscillatorNode.stop(this.#nextNoteTime + 0.03);
   }
 }
