871038bef4a1a24071d0587c9395fe953d97fb43
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2025-03-07 12:12:21 +0800

    Remove unneeded server options

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,7 @@ import type { UserConfig } from "vite";
 
 import htmlMinimize from "@sergeymakinen/vite-plugin-html-minimize";
 
-const serverOptions = {
-  host: "0.0.0.0",
-  port: 6001,
-  strictPort: true
-};
-
 export default <UserConfig>{
-  server: serverOptions,
-  preview: serverOptions,
   plugins: [
     htmlMinimize({
       minifierOptions: {
