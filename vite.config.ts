59e2902165c739324c395ca65bbe89f81d8cb480
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2025-08-15 09:43:30 +0800

    Use custom HTML minifier to fix build fail

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,47 @@
+import type { MinifierOptions } from "html-minifier-next";
 import type { UserConfig } from "vite";
 
-import htmlMinimize from "@sergeymakinen/vite-plugin-html-minimize";
+import { minify } from "html-minifier-next";
+import { createFilter } from "vite";
+
+const filter = createFilter("**/*.html");
+const options: MinifierOptions = {
+  collapseWhitespace: true,
+  html5: true,
+  minifyCSS: true,
+  minifyJS: true,
+  removeAttributeQuotes: true,
+  removeComments: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  useShortDoctype: true
+};
 
 export default <UserConfig>{
   plugins: [
-    htmlMinimize({
-      minifierOptions: {
-        collapseWhitespace: true,
-        html5: true,
-        minifyCSS: true,
-        minifyJS: true,
-        removeAttributeQuotes: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        useShortDoctype: true
+    {
+      // Yoinked from `@sergeymakinen/vite-plugin-html-minimize` with some
+      // changes.
+      name: "html-minifier",
+      apply: "build",
+      enforce: "post",
+      generateBundle: {
+        order: "post",
+        async handler(_, bundle) {
+          for (const assetOrChunk of Object.values(bundle)) {
+            if (
+              assetOrChunk.type === "asset" &&
+              filter(assetOrChunk.fileName)
+            ) {
+              assetOrChunk.source = await minify(
+                assetOrChunk.source.toString(),
+                options
+              );
+            }
+          }
+        }
       }
-    })
+    }
   ]
 };
